bf20f73f25d4a92082ecae8d5cd434393ff41c57
metamoorph <analomidze80@gmail.com>
2023-06-23 18:35:10 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function notifyMe() {
+    if (!("Notification" in window)) {
+     
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") {
+      
+      const notification = new Notification("Hi there!");
+      
+    } else if (Notification.permission !== "denied") {
+     
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          const notification = new Notification("Hi there!");
+          
+        }
+      });
+    }
+
+  }
